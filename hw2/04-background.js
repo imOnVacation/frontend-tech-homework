d1cebc335fc3d054c58d8136523bcf721e95c59c
Leshi Chen <leshi@pdx.edu>
2024-10-19 22:55:11 -0700

    complete e04

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,36 @@
-// Add your code here
+const button = document.querySelector('input[type="button"]');
+const input = document.getElementById('inputSecond');
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const a = Math.random() * 0.5 + 0.3;
+  return `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomColor();
+}
+
+const handleClick = (() => {
+  let intervalId = null;
+  let isRunning = false;
+
+  return function () {
+    let intervalSeconds = Number(input.value);
+
+    if (!isRunning) {
+      changeBackgroundColor();
+      intervalId = setInterval(changeBackgroundColor, intervalSeconds * 1000);
+      button.value = 'Stop';
+      button.classList.add('btn-danger');
+    } else {
+      clearInterval(intervalId);
+      button.value = 'Start';
+    }
+    isRunning = !isRunning;
+  };
+})();
+
+button.addEventListener('click', handleClick);
